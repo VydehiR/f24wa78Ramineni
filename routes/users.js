0cc75b3fe4399242ebc380cdcbf404a27dc3d36a
VydehiR <vydehi1303@gmail.com>
2024-10-30 16:36:18 -0500

    changes made

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
 var value = 0;
 var total = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  value +=2;
-  total+= value;
-  res.send('respond with a resource');
+  value += 2;
+  total += value;
+  res.send(`Total is ${total}`);
 });
+
+module.exports = router;
