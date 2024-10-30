69cfa3c1ab6d53135ea2ef3532e9f2f46de41797
VydehiR <vydehi1303@gmail.com>
2024-10-30 16:14:45 -0500

    new endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var value = 0;
+var total = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  value +=2;
+  total+= value;
   res.send('respond with a resource');
 });
-
-module.exports = router;
